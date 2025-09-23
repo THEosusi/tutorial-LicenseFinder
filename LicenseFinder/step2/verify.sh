0be0021c03e015502c160e7a9f581bca29d95051
rifat4 <rifat_94@hotmail.com>
2025-09-23 17:30:56 +0200

    add: step 2 install license_finder

--- LicenseFinder/step2/verify.sh
+++ LicenseFinder/step2/verify.sh
@@ -1,1 +1,10 @@
-stat /tmp/step2
+#!/bin/bash
+# verify.sh for Step 2
+
+if license_finder --version >/dev/null 2>&1 || which license_finder >/dev/null 2>&1; then
+  echo "LicenseFinder is installed ✅"
+  exit 0
+else
+  echo "LicenseFinder is not installed ❌ Please install Ruby before proceeding."
+  exit 1
+fi
