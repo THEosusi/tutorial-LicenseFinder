e29aa8b73b8be87ec51357f5e0d0602f61746920
rifat4 <rifat_94@hotmail.com>
2025-09-26 13:09:49 +0200

    fix: moved around so we get verification feedback in console

--- LicenseFinder/step2/verify.sh
+++ LicenseFinder/step2/verify.sh
@@ -2,9 +2,9 @@
 # verify.sh for Step 2
 
 if license_finder --version >/dev/null 2>&1 || which license_finder >/dev/null 2>&1; then
-  echo "LicenseFinder is installed ✅"
+  # "LicenseFinder is installed ✅"
   exit 0
 else
-  echo "LicenseFinder is not installed ❌ Please install Ruby before proceeding."
+  # "LicenseFinder is not installed ❌ Please install Ruby before proceeding."
   exit 1
 fi
--- LicenseFinder/step3/foreground.sh
+++ LicenseFinder/step3/foreground.sh
@@ -1,2 +1,2 @@
 while [ ! -f /tmp/background2 ]; do sleep 1; done
-echo echo "Ruby is installed ✅"
+echo "LicenseFinder is installed ✅"
