bc2315b37fbb3e86d3e0ffb5236115a7ebaf124a
rifat4 <rifat_94@hotmail.com>
2025-09-28 16:55:32 +0200

    fix: verify shell not running in same directory, now checking if the file in the expected directory and place exists.

--- LicenseFinder/step3/verify.sh
+++ LicenseFinder/step3/verify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # verify.sh for Step 3
 
-if [ -f "$PWD/pom.xml" ]; then
+if [ -f "/root/simple-java-maven-app/pom.xml" ]; then
   echo "Project cloned"
   exit 0
 else
-  echo "Project not found, Please clone and then cd into the repository."
+  echo "Project not found. Please clone and cd into the repository."
   exit 1
 fi
