61678022785c5185e059c6a8d74bc38a39139e2e
rifat4 <rifat_94@hotmail.com>
2025-10-01 02:08:12 +0200

    fix: formatting for the if else statement

--- LicenseFinder/step3.1/verify.sh
+++ LicenseFinder/step3.1/verify.sh
@@ -5,4 +5,8 @@ CURRENT_ENV=$(conda info --envs 2>/dev/null | grep '\*' | awk '{print $1}')
 PYTHON_VERSION=$(python --version 2>&1 | awk '{print $2}')
 PIP_VERSION=$(pip --version 2>/dev/null | awk '{print $2}')
 
-[ "$CURRENT_ENV" = "py310" ] && [ "$PYTHON_VERSION" = "3.10.13" ] && [ "$PIP_VERSION" = "21.3.1" ] && exit 0 || exit 1
+if [ "$CURRENT_ENV" = "py310" ] && [ "$PYTHON_VERSION" = "3.10.13" ] && [ "$PIP_VERSION" = "21.3.1" ]; then
+ exit 0
+else
+  exit 1
+fi
