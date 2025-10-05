e431455486c3c9c15179452c9ba3d9a80986789c
rifat4 <rifat_94@hotmail.com>
2025-10-05 19:45:47 +0200

    fix: testing fix to the easter egg

--- LicenseFinder/intro/foreground.sh
+++ LicenseFinder/intro/foreground.sh
@@ -2,3 +2,10 @@ echo waiting for initial setting to finish
 while [ ! -f /tmp/background10 ]; do sleep 1; done
 source ~/.bashrc
 echo Welcome to LicenseFinder tutorial!
+
+printf "     .     '     ,"
+printf "       _________"
+printf "    _ /_|_____|_\\ _"
+printf "      '. \\   / .'"
+printf "        '.\\ /.'"
+printf "          '.'     💎 Ruby is ready. Let the gems flow!"
