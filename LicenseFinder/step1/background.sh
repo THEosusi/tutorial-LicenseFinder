cd30c4138071022b33ae358efc7a963f32fb3270
rifat4 <rifat_94@hotmail.com>
2025-10-05 21:52:16 +0200

    fix: trying to print art by saving it first instead.

---
+++ LicenseFinder/step1/background.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p /tmp/ascii_assets
+cat > /tmp/ascii_assets/ruby_ascii.txt << "EOF"
+ .     '     ,
+   _________
+_ /_|_____|_\ _
+  '. \   / .'
+    '.\ /.'
+      '.'     💎 Ruby is ready. Let the gems flow!
+EOF
+# You can add more files here if you have multiple ASCII arts
--- LicenseFinder/step2/foreground.sh
+++ LicenseFinder/step2/foreground.sh
@@ -1,9 +1,2 @@
 while [ ! -f /tmp/background2 ]; do sleep 1; done
-set +v
-echo "     .     '     ,"
-echo "       _________"
-echo "    _ /_|_____|_\\ _"
-echo "      '. \\   / .'"
-echo "        '.\\ /.'"
-echo "          '.'     💎 Ruby is ready. Let the gems flow!"
-set -v
+cat /tmp/ascii_assets/ruby_ascii.txt
